55f21a6d18288aa3ed6bb228f9822542c0c9bb20
hachulwoo <hachulwoo5@naver.com>
2022-12-22 01:20:13 +0900

    Holo Moonshin

---
+++ holo_moonshin/Assets/#Script/ColorChanegeBox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanegeBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Renderer squareColor;
+    void Start()
+    {
+        squareColor = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag=="Sphere")
+        {
+            squareColor.material.color = Color.red;
+        }
+        if (other.transform.tag == "Sphere2")
+        {
+            squareColor.material.color = Color.blue;
+        }
+    }
+}
